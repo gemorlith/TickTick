f476c11e082f7b68d2cb03e1d44cba8ae28efdbb
CallitMez <lhekkers054@gmail.com>
2016-11-03 21:33:02 +0100

    fixte het vuurtje

--- 30_FinishingGame/TickTickFinal/GameManagement/gameobjects/enemies/UnpredictableEnemy.cs
+++ 30_FinishingGame/TickTickFinal/GameManagement/gameobjects/enemies/UnpredictableEnemy.cs
@@ -6,12 +6,12 @@ class UnpredictableEnemy : PatrollingEnemy
     public override void Update(GameTime gameTime)
     {
         base.Update(gameTime);
-        if (waitTime > 0 || GameEnvironment.Random.NextDouble() > 0.01)
+        if (waitTime > 0 || GameEnvironment.Random.NextDouble() > 0.01 || inJump)
         {
             return;
         }
         TurnAround();
-        velocity.X = Math.Sign(velocity.X) * (float)GameEnvironment.Random.NextDouble() * 5.0f;       
+        velocity.X = velocity.X * (float)GameEnvironment.Random.NextDouble() * 3.0f;       
     }
 }
 
